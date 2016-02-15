094ba64b7870d283e359ebed3e133b973399931a
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-02-15 16:31:58 +0100

    point 04 done

---
+++ D1/src/list_tools.f90
@@ -0,0 +1,18 @@
+module list_tools
+
+  implicit none
+  integer :: j, check = 0
+
+contains
+  integer function is_sorted(arr)
+    real, dimension(:) :: arr
+    do j=1, size(arr)-1
+       if( arr(j) > arr(j+1)) then
+          check = 10
+       end if
+    end do
+    
+    is_sorted = check
+  end function is_sorted
+
+end module
